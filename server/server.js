fe6e7c0410957995962fa18d0001f20542e5c145
sanjaymohansingh <sanjaysingh19p@gmail.com>
2023-12-26 05:09:06 -0800

    setting up serverjs file

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const path = require("path");
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.listen(5001, () => {
+  console.log(`App Running on Port Number : 5001`);
+});
